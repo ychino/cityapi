a2344d6e3522a3adb165d33a0bcca67c936177d7
Yuki Chino <yuki.chino@bracketglobal.com>
2018-11-27 14:58:01 +0900

    Add a route for DELETE

--- cityapi/Controllers/PointsOfInterestController.cs
+++ cityapi/Controllers/PointsOfInterestController.cs
@@ -177,5 +177,25 @@ namespace cityapi.Controllers
 
             return NoContent();
         }
+
+        [HttpDelete("{cityId}/pointsOfinterest/{id}")]
+        public IActionResult DeletePointOfInterest(int cityId, int id)
+        {
+            var city = CitiesDataStore.Current.Cities.FirstOrDefault(c => c.Id == cityId);
+            if (city == null)
+            {
+                return NotFound();
+            }
+
+            var pointOfInterestFromStore = city.PointsOfInterest.FirstOrDefault(c => c.Id == id);
+            if (pointOfInterestFromStore == null)
+            {
+                return NotFound();
+            }
+
+            city.PointsOfInterest.Remove(pointOfInterestFromStore);
+
+            return NoContent();
+        }
     }
 }
