0248f7f6b114a66e7ffc5a2f42f23efb492f65a4
Yuki Chino <yuki.chino@bracketglobal.com>
2018-11-28 11:38:11 +0900

    Add Entity classes

---
+++ cityapi/Entities/PointOfInterest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace cityapi.Entities
+{
+    public class PointOfInterest
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
+
+        [ForeignKey("CityId")]
+        public City City { get; set; }
+        public int CityId { get; set; }
+    }
+}
