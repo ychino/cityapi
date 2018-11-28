3d461ca44a42f746cfc403fe5424a0b133fca185
Yuki Chino <yuki.chino@bracketglobal.com>
2018-11-28 14:40:32 +0900

    Add migration files and recreate a db

--- cityapi/Entities/CityInfoContext.cs
+++ cityapi/Entities/CityInfoContext.cs
@@ -10,7 +10,7 @@ namespace cityapi.Entities
     {
         public CityInfoContext(DbContextOptions<CityInfoContext> options) : base(options)
         {
-            Database.EnsureCreated();
+            Database.Migrate();
         }
         public DbSet<City> Cities { get; set; }
         public DbSet<PointOfInterest> PointOfInterest { get; set; }
